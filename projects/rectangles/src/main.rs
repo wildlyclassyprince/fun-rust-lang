805f91c230bb7e50e3e442ae0f2a80fd40e4a486
wildlyclassyprince <mthuli.bhebhe@gmail.com>
2024-06-06 11:31:55 +0200

    completed using structs and structure related data

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -3,9 +3,17 @@ fn main() {
         length: 10,
         width: 5,
     };
+    let rectangle2 = Rectangle {
+        length: 9,
+        width: 4,
+    };
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rectangle)
+        rectangle.area()
+    );
+    println!(
+        "Can the second rectangle fit in the first? {}",
+        rectangle.can_hold(&rectangle2)
     );
 
     // Debugging
@@ -13,6 +21,11 @@ fn main() {
 
     // Takes ownership of an expression, as opposed to println! which takes a reference
     dbg!(&rectangle);
+
+    // This block shows how associated functions work
+    let square = Rectangle::square(5);
+    dbg!(&square);
+    println!("Area of the square is {}", square.area());
 }
 
 #[derive(Debug)] //needs to implemented above the object we want to debug
@@ -21,6 +34,17 @@ struct Rectangle {
     width: u32,
 }
 
-fn area(dimensions: &Rectangle) -> u32 {
-    dimensions.length * dimensions.width
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+    fn square(size: u32) -> Self {
+        Self {
+            length: size,
+            width: size,
+        }
+    }
 }
